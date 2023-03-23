eba2d6a64ce30a0b53e01c3c959b0f3352404ca5
Dennis Mbugua <dennis@starlaunch.org>
2023-03-23 19:47:40 +0300

    Update context_processors.py

--- myshop/cart/context_processors.py
+++ myshop/cart/context_processors.py
@@ -1,5 +1,8 @@
-from .cart import Cart
 
+# set the current cart into the request context. We will be able to access the cart in any template.
+# Context processors can reside anywhere in your code, but creating them here will keep your code well organized
+
+from .cart import Cart
 
 def cart(request):
     return {'cart': Cart(request)}
