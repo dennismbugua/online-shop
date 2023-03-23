b2849be58bd591882bc046ec1b40aa14ee12ae23
Dennis Mbugua <dennis@starlaunch.org>
2023-03-23 19:53:27 +0300

    Update views.py

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -4,7 +4,6 @@ from django.views.decorators.http import require_POST
 from .models import Coupon
 from .forms import CouponApplyForm
 
-
 @require_POST
 def coupon_apply(request):
     now = timezone.now()
@@ -20,3 +19,13 @@ def coupon_apply(request):
         except Coupon.DoesNotExist:
             request.session['coupon_id'] = None
     return redirect('cart:cart_detail')
+
+
+
+
+# The coupon_apply view validates the coupon and stores it in the user's session. We apply the require_POST decorator to this view to restrict it to POST requests. In the view, we perform the following tasks:
+
+# 1 We instantiate the CouponApplyForm form using the posted data and we check that the form is valid.
+# 2 If the form is valid, we get the code entered by the user from the form's cleaned_data dictionary. We try to retrieve the Coupon object with the given code. We use the iexact field lookup to perform a case-insensitive exact match. The coupon has to be currently active (active=True) and valid for the current datetime. We use Django's timezone.now() function to get the current time zone-aware datetime and we compare it with the valid_from and valid_to fields performing lte (less than or equal to) and gte (greater than or equal to) field lookups, respectively.
+# 3 We store the coupon ID in the user's session.
+# 4 We redirect the user to the cart_detail URL to display the cart with the coupon applied.
