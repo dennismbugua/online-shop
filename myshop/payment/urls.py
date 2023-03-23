1b1ccd69e20fdddf2f578890c5c7f2ce3c531aed
Dennis Mbugua <dennis@starlaunch.org>
2023-03-23 20:10:56 +0300

    Update urls.py

--- myshop/payment/urls.py
+++ myshop/payment/urls.py
@@ -6,7 +6,10 @@ from django.utils.translation import gettext_lazy as _
 app_name = 'payment'
 
 urlpatterns = [
+    # view that processes the payment
     path(_('process/'), views.payment_process, name='process'),
+    # view to redirect the user if the payment is successful
     path(_('done/'), views.payment_done, name='done'),
+    # view to redirect the user if the payment is not successful
     path(_('canceled/'), views.payment_canceled, name='canceled'),
 ]
