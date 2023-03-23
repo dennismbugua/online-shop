33194bddbcdfd83fdb36f7ce3aae68d3c7ad8fe4
Dennis Mbugua <dennis@starlaunch.org>
2023-03-23 19:54:04 +0300

    Update __init__.py

--- myshop/myshop/__init__.py
+++ myshop/myshop/__init__.py
@@ -1,2 +1,5 @@
 # import celery
 from .celery import app as celery_app
+
+
+# You need to import the celery module in the __init__.py file of your project to make sure it is loaded when Django starts
