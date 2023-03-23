07d2cd3edc0aff0160114c15d7a5ccc1b8f485ec
Dennis Mbugua <dennis@starlaunch.org>
2023-03-23 20:15:44 +0300

    Update admin.py

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -9,6 +9,11 @@ class CategoryAdmin(TranslatableAdmin):
 
     def get_prepopulated_fields(self, request, obj=None):
             return {'slug': ('name',)}
+        
+# use the prepopulated_fields attribute to specify fields where
+# the value is automatically set using the value of other fields.
+# As you have seen before, this is convenient for generating slugs.
+
 
 
 @admin.register(Product)
@@ -20,3 +25,11 @@ class ProductAdmin(TranslatableAdmin):
 
     def get_prepopulated_fields(self, request, obj=None):
             return {'slug': ('name',)}
+
+        
+# use the list_editable attribute in the ProductAdmin class to
+# set the fields that can be edited from the list display page
+# of the administration site. This will allow you to edit
+# multiple rows at once. Any field in list_editable must also be
+# listed in the list_display attribute since only the fields
+# displayed can be edited.
